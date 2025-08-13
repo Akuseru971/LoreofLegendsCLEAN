ac7053c106f5073a31975bbbed062f69c568da24
Akuseru971 <axellvalentino@gmail.com>
2025-08-13 15:44:59 +0200

    Style update: Elegant dark fantasy font for generated lore

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css';
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Police élégante */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Cormorant+Garamond:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
